5e65ef21c336d268bbf4c94ad0621472c9553646
Vipul Chhabra <vipulchhabra555@gmail.com>
2020-09-24 12:35:25 +0530

    Minor Update

--- MiniWorld.py
+++ MiniWorld.py
@@ -108,15 +108,14 @@ while(1):
 
         tmp = input("Enter any key to CONTINUE>")
 
-        with con:
-            cur = con.cursor()
+        with con.cursor() as cur:
             while(1):
                 tmp = sp.call('clear', shell=True)
                 # Here taking example of Employee Mini-world
-                print("1. Option 1") # Hire an Employee
-                print("2. Option 2") # Fire an Employee
-                print("3. Option 3") # Promote Employee
-                print("4. Option 4") # Employee Statistics
+                print("1. Option 1")  # Hire an Employee
+                print("2. Option 2")  # Fire an Employee
+                print("3. Option 3")  # Promote Employee
+                print("4. Option 4")  # Employee Statistics
                 print("5. Logout")
                 ch = int(input("Enter choice> "))
                 tmp = sp.call('clear', shell=True)
